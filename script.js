99c92997c95b083f7f7eff31f92c2b6223e114a1
gaspalex2011@gmail.com <gaspalex2011@gmail.com>
2023-06-11 19:21:53 +0300

    second

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const canvas = document.querySelector('canvas')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+// canvas.width = 1
+// canvas.height = 1
+
 const c = canvas.getContext('2d')
 // c.fillStyle = "rgba(255, 0, 0, 0.5)"
 // c.fillRect(100, 100, 100, 100)
@@ -19,9 +22,9 @@ const c = canvas.getContext('2d')
 // c.strokeStyle = "blue"
 // c.stroke()
 
-const getRandomColorNum = () => {
-  return Math.floor(Math.random() * 255);
-}
+// const getRandomColorNum = () => {
+//   return Math.floor(Math.random() * 255);
+// }
 
 // const { innerWidth, innHeight } = window// for (let i = 0; i < 50; i++) {
 //   const x = Math.random() * innerWidth
@@ -35,7 +38,12 @@ const getRandomColorNum = () => {
 //   c.stroke()
 // }
 
-const possibleColors = [
+const mouse = {
+  x: undefined,
+  y: undefined,
+}
+
+const possibleColors1 = [
   '#FF5F5D',
   '#3F7C85',
   '#00CCBF',
@@ -43,30 +51,75 @@ const possibleColors = [
   '#747E7E',
 ]
 
+const possibleColors2 = [
+  '#662400',
+  '#FF6B1A',
+  '#B33F00',
+  '#006663',
+  '#00B3AD',
+]
+
+const possibleColors3 = [
+  '#5b4e07',
+  '#978839',
+  '#c6b34e',
+  '#f3e6a1',
+  '#36373e',
+]
+
+const possibleColors4 = [
+  '#22A699',
+  '#F2BE22',
+  '#F29727',
+  '#F24C3D',
+]
+
+const possibleColors5 = [
+  '#FF78C4',
+  '#E1AEFF',
+  '#FFBDF7',
+  '#FFECEC',
+]
+
+const colorPalettes = [possibleColors1, possibleColors2, possibleColors3, possibleColors4, possibleColors5]
+const mouseDist = 70
 class Circle {
   x = 1
   y = 1
   radius = 1
-  color = possibleColors[Math.floor(Math.random() * possibleColors.length + 1)]
+  initialRadius = 1
+  palette = possibleColors1
+  colorIndex = Math.floor(Math.random() * (this.palette.length + 1))
+  canBeDestroyed = false
+  destroyed = false
 
   constructor (id) {
     this.id = id
-    this.radius = Math.random() * 3 + 1
+    this.radius = Math.random() * 6 + 1
+    this.initialRadius = this.radius
     this.x = Math.random() * (innerWidth - 2 * this.radius + 1)
     this.y = Math.random() * (innerHeight - 2 * this.radius + 1)
-    this.dx = (Math.random() * 0.8 + 1 ) * (Math.random() >= 0.5 ? 1 : -1)
-    this.dy = (Math.random() * 0.8 + 1 ) * (Math.random() >= 0.5 ? 1 : -1)
+    this.dx = (Math.random() * 0.3 + 1 ) * (Math.random() >= 0.5 ? 1 : -1)
+    this.dy = (Math.random() * 0.3 + 1 ) * (Math.random() >= 0.5 ? 1 : -1)
+    this.color = this.palette[this.colorIndex]
   }
 
   draw () {
     const { x, y, radius } = this
-    c.beginPath()
-    c.arc(x, y, radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
+    try {
+
+      c.beginPath()
+      c.arc(x, y, radius, 0, Math.PI * 2, false)
+      c.fillStyle = this.color
+      c.fill()
+    } catch (e) {
+      console.log(e)
+    }
+
   }
 
   update () {
+    if (this.destroyed) return 
 
     const { x, y, radius} = this
     if (x <= radius || x + radius >= innerWidth) this.dx = -this.dx
@@ -74,7 +127,27 @@ class Circle {
     this.x += this.dx
     this.y += this.dy
 
+    const isNearMouse = (Math.abs(x - mouse.x) <= mouseDist) && Math.abs(y - mouse.y) <= mouseDist
+    if (isNearMouse) {
+      if (this.radius <= 100) this.radius += 4
+
+      this.canBeDestroyed = true
+    } else if (this.radius >= this.initialRadius){
+      this.radius = this.initialRadius < this.radius - 4 ?  this.radius - 4 : this.initialRadius
+    }
+
+    if (!isNearMouse) this.canBeDestroyed = false
     this.draw()
+  }
+
+  changeColor (newColor) {
+    this.color = newColor
+  }
+
+  destroy () {
+    if (!this.canBeDestroyed) return
+    this.radius = 0
+    this.destroyed = true
   }
 }
 const circleArr = []
@@ -90,3 +163,59 @@ const animate = () => {
   }
 }
 animate()
+
+const selectChangeHandler = (event) => {
+  const { selectedIndex } = event
+  const val = Number(event[selectedIndex].value)
+  const palette = colorPalettes[val - 1]
+  circleArr.forEach(circle => {
+    const i = circle.colorIndex
+    const newColor = palette[i] || palette[Math.floor(Math.random() * 5)]
+    circle.changeColor(newColor)
+  })
+}
+
+window.addEventListener('mousemove', (event) => {
+  const { clientX, clientY } = event
+  mouse.x = clientX
+  mouse.y = clientY
+})
+
+window.addEventListener('click', (event) => {
+  for (let circle of circleArr) {
+    circle.destroy()
+  }
+})
+
+const clickFakeSelect = () => {
+  const dropdown = document.querySelector('.fake-select-dropdown')
+  dropdown.classList.toggle("is-hidden");
+}
+
+document.querySelectorAll('.fake-option-wrapper').forEach(optionWrapper => {
+  optionWrapper.addEventListener('click', (event) => {
+    const { value } = optionWrapper.dataset
+    // change the visible option
+    document.querySelector('.fake-select').style.background = getComputedStyle(document.querySelector(`.fake-option-${value}`)).backgroundImage
+
+    const val = Number(value)
+    const palette = colorPalettes[val - 1]
+    circleArr.forEach(circle => {
+      const i = circle.colorIndex
+      const newColor = palette[i] || palette[Math.floor(Math.random() * 5)]
+      circle.changeColor(newColor)
+    })
+
+    const dropdown = document.querySelector('.fake-select-dropdown')
+    dropdown.classList.add("is-hidden");
+  })
+})
+
+const closeModal = () => {
+  document.querySelector('.modal').classList.remove('is-active')
+}
+
+// // style the options
+// document.querySelectorAll('option').forEach(option => {
+//   option.background = 'linear-gradient(0deg, #fdcd3b 50%, #ffed4b 50%)'
+// })
